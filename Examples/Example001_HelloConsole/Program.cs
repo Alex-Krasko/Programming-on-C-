982bdc3a4711c2773a7d0287826f5f76c8c7830a
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-10-25 21:23:55 +0300

    Added folders

---
+++ Examples/Example001_HelloConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿double a,b,c,d,s;
+Console.Write("A: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("B: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("C: ");
+c = Convert.ToInt32(Console.ReadLine());
+Console.Write("D: ");
+d = Convert.ToInt32(Console.ReadLine());
+
+s = (a+b+c+d)/4;
+
+Console.Write("Average = ");
+Console.WriteLine(s);
