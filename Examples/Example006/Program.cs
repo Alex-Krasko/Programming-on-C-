75301d49c5cb5b9387b1e484e54e48ad1332a541
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-10-28 21:37:46 +0300

    Added Examples 004, 005, 006

---
+++ Examples/Example006/Program.cs
@@ -0,0 +1,9 @@
+﻿int max(int a, int b, int c)
+{
+    int result;
+    if (a>b)  result = a; else result = b;
+    if (result>c) {} else result=c;
+    return result;
+}
+
+Console.WriteLine(max(100,80,60));
