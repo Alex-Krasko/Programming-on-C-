7dbd5cfd11123dc3461e8468dfecd82870f52690
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-03 13:24:20 +0300

    Example009 added

---
+++ Examples/Example009/Program.cs
@@ -0,0 +1,19 @@
+﻿string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+string newText = text;
+
+string Replace(char oldValue, char newValue)
+{
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(newText[i] == oldValue) newText = newText.Replace(oldValue, newValue);
+    }
+
+    return newText;
+}
+Console.WriteLine("Origin: "+text);
+Console.WriteLine(Replace('к', 'К'));
+Console.WriteLine(Replace(' ', '*'));
